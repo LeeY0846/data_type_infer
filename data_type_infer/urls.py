c01610711b183c463fc3ea103adfc35d80777afa
Yang <ylee091126@gmail.com>
2024-11-13 15:52:43 +1100

    Init vite project

--- data_type_infer/urls.py
+++ data_type_infer/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from data_type_infer_api import urls as data_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('data/', include(data_urls)),
 ]
--- data_type_infer_api/models.py
+++ data_type_infer_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class DataFile(models.Model):
+  filename = models.CharField(max_length=180)
+  update_date = models.DateTimeField("date updated")
+  
+class ColumnDataType(models.Model):
+  column_id = models.IntegerField()
+  column_type = models.CharField(max_length=40)
+  file = models.ForeignKey(DataFile, on_delete=models.CASCADE)
